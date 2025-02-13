bea816b4e8a2f72055ffd21bcccdc41dbfdc56c2
skierua <iurii.vasurchak@gmail.com>
2025-02-14 00:20:55 +0200

    switch from test api

--- src/path.js
+++ src/path.js
@@ -6,8 +6,8 @@
 // for deployment
 // const PATH_TO_SERVER = "https://board.kantorfk.com/api/v3"; // for deployment
 // const PATH_TO_SSE = "https://board.kantorfk.com/api/v3/sse"; // for deployment
-const AUTH_PATH = "https://test.kantorfk.com/api/v4/auth"; // for deployment
-const API_PATH = "https://test.kantorfk.com/api/v4"; // for deployment
+const AUTH_PATH = "https://kantorfk.com/api/v4/auth"; // for deployment
+const API_PATH = "https://kantorfk.com/api/v4"; // for deployment
 // const SSE_PATH = "https://test.kantorfk.com/api/v4/sse"; // for deployment testings
 
 export { API_PATH, AUTH_PATH };
